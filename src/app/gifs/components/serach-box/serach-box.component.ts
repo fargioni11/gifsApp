0e8b739c13c773d6711cd3f8fe67873d44ce7914
Adriel Fargioni <adrielfargioni@gmail.com>
2023-05-28 15:59:40 -0300

    servicio gifs validaciones

--- src/app/gifs/components/serach-box/serach-box.component.ts
+++ src/app/gifs/components/serach-box/serach-box.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-serach-box',
   templateUrl: './serach-box.component.html',
-  styleUrls: ['./serach-box.component.css']
 })
 export class SerachBoxComponent {
+  @ViewChild('txtTagInput')  
+  public tagInput!: ElementRef<HTMLInputElement>
+ constructor() {
+   
+ }
 
+ searchTag() {
+  const newTag = this.tagInput.nativeElement.value;
+   console.log({newTag});
+ }
 }
